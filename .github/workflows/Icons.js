243d5d7da01d671452ac721c7ba2f9d78a3bca8b
jkamkk <144686850+jkamkk@users.noreply.github.com>
2024-08-21 19:27:54 +0800

    Update Icons.js

--- .github/workflows/Icons.js
+++ .github/workflows/Icons.js
@@ -5,14 +5,49 @@
       "name": "绝区零logo",
       "url": "https://files.catbox.moe/ni7u9i.png"
     },
-       {
+    {
       "name": "绝区零_雅",
       "url": "https://files.catbox.moe/2j6pxx.png"
     },
-     {
+    {
       "name": "btc",
       "url": "https://files.catbox.moe/agrq5e.png"
     },
-    
+    {
+      "name": "osu!",
+      "url": "https://files.catbox.moe/ec6lna.png"
+    },
+    {
+      "name": "sensei",
+      "url": "https://files.catbox.moe/73gxxa.png"
+    },
+    {
+      "name": "芽衣_呼呼呼",
+      "url": "https://files.catbox.moe/gycqwf.png"
+    },
+    {
+      "name": "芽衣_加载0",
+      "url": "https://files.catbox.moe/y1c2qd.png"
+    },
+    {
+      "name": "苹果返回键",
+      "url": "https://files.catbox.moe/y0on7a.png"
+    },
+    {
+      "name": "C4D",
+      "url": "https://files.catbox.moe/a27inc.png"
+    },
+    {
+      "name": "原神派蒙",
+      "url": "https://files.catbox.moe/rt0if4.png"
+    },
+    {
+      "name": "保护伞",
+      "url": "https://files.catbox.moe/g6sy1k.png"
+    },
+    {
+      "name": "苹果设置",
+      "url": "https://files.catbox.moe/f9k0hz.png"
+    },
   ]
 }
